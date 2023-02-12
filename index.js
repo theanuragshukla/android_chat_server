5c0355006fca1a12f678bd0040e1a9c255c76ca6
theanuragshukla <tachyonanurag@gmail.com>
2023-02-12 16:10:20 +0530

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const http = require("http").Server(app);
 const bcrypt = require('bcryptjs')
 const saltRounds = 10;
